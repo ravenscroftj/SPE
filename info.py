ac0cde457bf10988270c9cf5cbf98f93c6855def
stani <stani@be77ae6e-66fb-0310-8b80-9b5aebdb08cc>
2008-08-30 14:09:45 +0000

    upgrade version number
    git-svn-id: svn://svn.berlios.de/python/spe/trunk/_spe@348 be77ae6e-66fb-0310-8b80-9b5aebdb08cc

--- info.py
+++ info.py
@@ -70,7 +70,7 @@ INFO={
     'title'             : "SPE",
     'url'               : 'http://pythonide.stani.be',
     'userPath'          : sm.osx.userPath('.spe'),
-    'version'           : "0.8.4.h",
+    'version'           : "0.8.4.i",
     'wxVersion'         : "2.6.1.0.",
 }
 
